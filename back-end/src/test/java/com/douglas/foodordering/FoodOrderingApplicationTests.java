a3293505afe3f142749a2768a671a9a0294b0eaf
quangle <quang.le205@gmail.com>
2019-01-26 16:17:26 -0800

    Initial Spring framework for REST API

---
+++ back-end/src/test/java/com/douglas/foodordering/FoodOrderingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.douglas.foodordering;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodOrderingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
