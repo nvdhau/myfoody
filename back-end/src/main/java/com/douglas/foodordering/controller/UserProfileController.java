5ecb947b0061177ca42088ba17c60d4dc3e212f0
Toan Thien Le <toan.cse2818@gmail.com>
2019-03-09 13:16:27 -0800

    Profile services

--- back-end/src/main/java/com/douglas/foodordering/controller/UserProfileController.java
+++ back-end/src/main/java/com/douglas/foodordering/controller/UserProfileController.java
@@ -1,5 +1,7 @@
 package com.douglas.foodordering.controller;
 
+import java.text.ParseException;
+import java.util.HashMap;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.douglas.foodordering.domain.UserProfile;
 import com.douglas.foodordering.exceptionhandler.ControllerException;
 import com.douglas.foodordering.service.UserProfileService;
+import com.douglas.foodordering.utilities.Utils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class UserProfileController {
@@ -34,14 +38,37 @@ public class UserProfileController {
 	}
 	
 	@PutMapping("/api/user/{user_id}/profile")
-	public ResponseEntity<UserProfile> updateUserProfile(@PathVariable("user_id") Long userId, @RequestBody UserProfile reqBody) {
+	public ResponseEntity<UserProfile> updateUserProfile(@PathVariable("user_id") Long userId, @RequestBody String reqBody) {
 		
 		try {
-			UserProfile profile = userProfileService.updateUserProfile(userId, reqBody);	
-
+			HashMap<String, Object> requestBody =
+			        new ObjectMapper().readValue(reqBody, HashMap.class);			
+			
+			UserProfile inputProfile = convertToProfileObject(requestBody);
+			
+			UserProfile profile = userProfileService.updateUserProfile(userId, inputProfile);	
+			
 			return ResponseEntity.ok(profile);
 		} catch(Exception e) {
 			throw new ControllerException(e.getMessage());
 		}
+	}
+	
+	private UserProfile convertToProfileObject(HashMap<String, Object> requestBody) {
+		UserProfile profile = new UserProfile();
+		
+		profile.setEmail(requestBody.get("email").toString());
+		profile.setId(Long.valueOf(requestBody.get("id").toString()));
+		profile.setFirstname(requestBody.get("firstname").toString());
+		profile.setLastname(requestBody.get("lastname").toString());
+		profile.setAddress(requestBody.get("address").toString());
+		profile.setPhonenumbers(requestBody.get("phonenumbers").toString());			
+		try {
+			profile.setDateofbirth(Utils.parseStandardSQLDate(requestBody.get("dateofbirth").toString()));
+		} catch (ParseException e) {
+			throw new ControllerException(e.getMessage());
+		}
+		
+		return profile;
 	}	
 }
